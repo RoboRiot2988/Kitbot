448d88f306f50fc97ed65d85a6124972cc5f44f9
Roboriot <roboriot29881@gmail.com>
2026-02-11 12:21:00 -0800

    added information comment in Main.java

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -2,6 +2,10 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+
+// THIS CODE IS UNDER THE REPOSITORY "Kitbot" IN GITHUB. IT IS RUN UNDER THE SCHOOL ACCOUNT
+
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
