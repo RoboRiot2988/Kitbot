09ef6f13f88e8492b7f0f2996ff77931f6768ebd
Roboriot <roboriot29881@gmail.com>
2026-02-11 12:19:12 -0800

    removed test comment

--- src/main/java/frc/robot/commands/ExampleCommand.java
+++ src/main/java/frc/robot/commands/ExampleCommand.java
@@ -2,8 +2,6 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-//test to see which repository is being used for the code, and to see if the code is being updated on github
-
 package frc.robot.commands;
 
 import frc.robot.subsystems.ExampleSubsystem;
